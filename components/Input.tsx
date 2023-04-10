21da8e0d505e01ada03d19a09b9aae9f523a9983
einarbarkved <einar.barkved@gmail.com>
2023-04-10 15:31:38 +0200

    Added register functionality single page

--- components/Input.tsx
+++ components/Input.tsx
@@ -35,7 +35,20 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
       />
       <label
         htmlFor={id}
-        className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+        className="
+        absolute text-md 
+        text-zinc-400 
+        duration-150 transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-10 
+        origin-[0] 
+        left-6 
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3"
       >
         {label}
       </label>
