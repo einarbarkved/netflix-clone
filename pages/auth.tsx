4c48a3f103f6afa310b0a1a85cef1f377efc5e73
einarbarkved <einar.barkved@gmail.com>
2023-04-10 22:47:01 +0200

    Added register user functionality

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/Input";
 import React, { useCallback, useState } from "react";
+import axios from "axios";
 
 const auth = () => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,18 @@ const auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -54,7 +67,10 @@ const auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
