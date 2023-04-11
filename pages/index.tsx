7dc6c27b8a85ee0a86c195b5532f812e8b3a82ad
einarbarkved <einar.barkved@gmail.com>
2023-04-11 20:25:42 +0200

    Created navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
@@ -19,5 +20,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
