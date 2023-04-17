6eb9187941c961e55c3ba47c9f0c5b2eba298b72
einarbarkved <einar.barkved@gmail.com>
2023-04-17 22:27:21 +0200

    ESLint fixes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,43 +6,41 @@ import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
-const auth = () => {
+const useAuth = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [usePassword, setPassword] = useState("");
 
   const [variant, setVariant] = useState("login");
   const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) =>
-      currentVariant === "login" ? "register" : "login"
-    );
+    setVariant((currentVariant) => (currentVariant === "login" ? "register" : "login"));
   }, []);
 
   const login = useCallback(async () => {
     try {
       await signIn("credentials", {
         email,
-        password,
+        usePassword,
         callbackUrl: "/profiles",
       });
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, usePassword]);
 
   const register = useCallback(async () => {
     try {
       await axios.post("/api/register", {
         email,
         name,
-        password,
+        usePassword,
       });
 
       login();
     } catch (error) {
       console.log(error);
     }
-  }, [email, name, password, login]);
+  }, [email, name, usePassword, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -52,9 +50,7 @@ const auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign in" : "Register"}
-            </h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">{variant === "login" ? "Sign in" : "Register"}</h2>
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
@@ -82,7 +78,7 @@ const auth = () => {
                   setPassword(event.target.value);
                 }}
                 type="password"
-                value={password}
+                value={usePassword}
               />
             </div>
             <button
@@ -106,14 +102,9 @@ const auth = () => {
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
-              {variant === "login"
-                ? "First time using Netflix?"
-                : "Already have an account?"}
+              {variant === "login" ? "First time using Netflix?" : "Already have an account?"}
 
-              <span
-                onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer"
-              >
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                 {variant === "login" ? "Create an account" : "Login"}
               </span>
             </p>
@@ -124,4 +115,4 @@ const auth = () => {
   );
 };
 
-export default auth;
+export default useAuth;
