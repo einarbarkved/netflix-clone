ad8454d9dd9ebd8991adda2f49f773fb34156367
einarbarkved <einar.barkved@gmail.com>
2023-04-17 22:31:41 +0200

    Changed spacing

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -14,7 +14,7 @@ const Watch = () => {
       <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
         <AiOutlineArrowLeft onClick={() => router.push("/")} className="text-white cursor-pointer" size={40} />
         <p className="text-white text-1xl md:text-3xl font-bold">
-          <span className="font-light">Watching:</span>
+          <span className="font-light mr-2">Watching:</span>
           {data?.title}
         </p>
       </nav>
